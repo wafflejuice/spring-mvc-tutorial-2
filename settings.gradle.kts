594c87bb9ab3d5671458e7422e55794875b7b63a
wafflejuice <wafflejuice96@gmail.com>
2022-11-23 23:06:47 +0900

    project setting

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "spring-mvc-tutorial-2"
---
+++ src/main/kotlin/inflearn/tutorial/SpringMvcTutorial2Application.kt
@@ -0,0 +1,11 @@
+package inflearn.tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMvcTutorial2Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringMvcTutorial2Application>(*args)
+}
