d2242b008a77e0db1cb86c830d65dfe5524e3fb1
wafflejuice <wafflejuice96@gmail.com>
2022-12-03 23:14:05 +0900

    오류 코드와 메시지 처리1

--- validation/src/main/kotlin/hello/itemservice/web/validation/ValidationItemControllerV2.kt
+++ validation/src/main/kotlin/hello/itemservice/web/validation/ValidationItemControllerV2.kt
@@ -77,7 +77,7 @@ class ValidationItemControllerV2(
         return "redirect:/validation/v2/items/{itemId}"
     }
 
-    @PostMapping("/add")
+    //    @PostMapping("/add")
     fun addItemV2(
         @ModelAttribute item: Item,
         bindingResult: BindingResult,
@@ -137,6 +137,84 @@ class ValidationItemControllerV2(
                         null,
                         null,
                         "가격 * 수량은 10,000 이상이어야 합니다. 현재 값 = $resultPrice"
+                    )
+                )
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            return "validation/v2/addForm"
+        }
+
+        // 성공 로직
+
+        val savedItem = itemRepository.save(item)
+        redirectAttributes.addAttribute("itemId", savedItem.id)
+        redirectAttributes.addAttribute("status", true)
+        return "redirect:/validation/v2/items/{itemId}"
+    }
+
+    @PostMapping("/add")
+    fun addItemV3(
+        @ModelAttribute item: Item,
+        bindingResult: BindingResult,
+        redirectAttributes: RedirectAttributes,
+        model: Model
+    ): String {
+
+        // 검증 로직
+        if (StringUtils.hasText(item.itemName).not()) {
+            bindingResult.addError(
+                FieldError(
+                    "item",
+                    "itemName",
+                    item.itemName,
+                    false,
+                    arrayOf("required.item.itemName"),
+                    null,
+                    null
+                )
+            )
+        }
+        if (item.price == null || item.price!! < 1000 || item.price!! > 1_000_000) {
+            bindingResult.addError(
+                FieldError(
+                    "item",
+                    "price",
+                    item.price,
+                    false,
+                    arrayOf("range.item.price"),
+                    arrayOf(1000, 1000000),
+                    null
+                )
+            )
+
+        }
+        if (item.quantity == null || item.quantity!! >= 9999) {
+            bindingResult.addError(
+                FieldError(
+                    "item",
+                    "quantity",
+                    item.quantity,
+                    false,
+                    arrayOf("max.item.quantity"),
+                    arrayOf(9999),
+                    null
+                )
+            )
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증
+        if (item.price != null && item.quantity != null) {
+            val resultPrice = item.price!! * item.quantity!!
+            if (resultPrice < 10000) {
+                bindingResult.addError(
+                    ObjectError(
+                        "item",
+                        arrayOf("totalPriceMin"),
+                        arrayOf(10000, resultPrice),
+                        null
                     )
                 )
             }
