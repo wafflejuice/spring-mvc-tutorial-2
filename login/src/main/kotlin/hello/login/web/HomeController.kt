51de76e85b772b0e801f82af5600fc065cb11a1d
wafflejuice <wafflejuice96@gmail.com>
2022-12-08 23:49:25 +0900

    [로그인 처리1 - 쿠키, 세션] 홈 화면

--- login/src/main/kotlin/hello/login/web/HomeController.kt
+++ login/src/main/kotlin/hello/login/web/HomeController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping
 class HomeController {
     @GetMapping("/")
     fun home(): String {
-        return "redirect:/items"
+        return "home"
     }
 }
