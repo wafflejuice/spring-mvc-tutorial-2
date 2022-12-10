64eaf3f4be95f9b5f288be6c2f3b0b3b0e102138
wafflejuice <wafflejuice96@gmail.com>
2022-12-10 23:46:21 +0900

    [로그인 처리1 - 쿠키, 세션] 로그인 처리하기 - 쿠키 사용

--- login/src/main/kotlin/hello/login/web/HomeController.kt
+++ login/src/main/kotlin/hello/login/web/HomeController.kt
@@ -1,12 +1,32 @@
 package hello.login.web
 
+import hello.login.domain.member.MemberRepository
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.CookieValue
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HomeController {
-    @GetMapping("/")
+class HomeController(
+    private val memberRepository: MemberRepository
+) {
+    //    @GetMapping("/")
     fun home(): String {
         return "home"
+    }
+
+    @GetMapping("/")
+    fun homeLogin(
+        @CookieValue(name = "memberId", required = false) memberId: Long?,
+        model: Model
+    ): String {
+        memberId ?: return "home"
+
+        // login
+        val loginMember = memberRepository.findById(memberId) ?: return "home"
+
+        model.addAttribute("member", loginMember)
+
+        return "loginHome"
     }
 }
--- login/src/main/kotlin/hello/login/web/login/LoginController.kt
+++ login/src/main/kotlin/hello/login/web/login/LoginController.kt
@@ -6,7 +6,10 @@ import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
 import javax.validation.Valid
+import kotlin.concurrent.thread
 
 @Controller
 class LoginController(
@@ -14,11 +17,18 @@ class LoginController(
 ) {
     @GetMapping("/login")
     fun loginForm(@ModelAttribute("loginForm") form: LoginForm): String {
+        thread {
+
+        }
         return "login/loginForm"
     }
 
     @PostMapping("/login")
-    fun login(@Valid @ModelAttribute("loginForm") form: LoginForm, bindingResult: BindingResult): String {
+    fun login(
+        @Valid @ModelAttribute("loginForm") form: LoginForm,
+        bindingResult: BindingResult,
+        response: HttpServletResponse
+    ): String {
         if (bindingResult.hasErrors()) {
             return "login/loginForm"
         }
@@ -31,6 +41,23 @@ class LoginController(
         }
 
         // 로그인 성공 처리
+
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        val idCookie = Cookie("memberId", loginMember.id.toString())
+        response.addCookie(idCookie)
+
         return "redirect:/"
+    }
+
+    @PostMapping("/logout")
+    fun logout(response: HttpServletResponse): String {
+        extractCookie(response, "memberId")
+        return "redirect:/"
+    }
+
+    private fun extractCookie(response: HttpServletResponse, cookieName: String) {
+        val cookie = Cookie(cookieName, null)
+        cookie.maxAge = 0
+        response.addCookie(cookie)
     }
 }
